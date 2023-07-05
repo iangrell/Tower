2387e28527a49e45f50ede0e19f5e0fabf098759
iangrell <igrell27@outlook.com>
2023-07-05 13:08:09 -0600

    tower

--- Tower.client/src/components/TowerEventCard.vue
+++ Tower.client/src/components/TowerEventCard.vue
@@ -2,9 +2,12 @@
     <router-link :to="{ name: 'TowerEventDetails', params: { towerEventId: towerEvent.id } }">
         <div class="elevation-3 rounded">
             <img class="img-fluid" :src="towerEvent.coverImg" :alt="towerEvent.name">
-            <div class="p-2">
-                <h5 class="text-light">{{ towerEvent.name }} <b v-if="towerEvent.isCanceled == true"
-                        class="bg-danger p-1 rounded">CANCELED</b></h5>
+            <div class="row p-2 d-flex justify-content-between">
+                <div><h5 class="text-light">{{ towerEvent.name }} </h5>
+                </div>
+                <div><b v-if="towerEvent.isCanceled == true"
+                        class="bg-danger p-1 rounded">CANCELED</b>
+                </div>
             </div>
         </div>
     </router-link>
